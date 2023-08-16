c0ce3528a15c582d5b287025bb8a4b17379e33c4
Md-arefin <ansariarefin617@gmail.com>
2023-08-16 18:07:36 +0600

    11 commit

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import http from 'http';
 import { Server } from 'socket.io';
 import { v4 as uuIdv4 } from 'uuid';
 import express from 'express';
+import cors from 'cors';
 
 const app = express();
 
